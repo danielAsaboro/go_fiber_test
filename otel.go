5483e99f3a32b0c91bca97ffaca068965747a9c9
Daniel Asaboro <asaborodaniel@gmail.com>
2024-10-22 09:19:25 +0100

    chore: move to env setup for otel

--- otel.go
+++ otel.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/resource"
@@ -10,10 +11,15 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 )
 
+var (
+	otlpEndpoint = os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	serviceName = os.Getenv("OTEL_SERVICE_NAME")
+)
+
 // InitTracerProvider initializes and returns a trace provider
 func InitTracerProvider() *sdktrace.TracerProvider {
 	exporter, err := otlptracegrpc.New(context.Background(),
-		otlptracegrpc.WithEndpoint("localhost:4317"),
+		otlptracegrpc.WithEndpoint(otlpEndpoint),
 		otlptracegrpc.WithInsecure(),
 	)
 	if err != nil {
@@ -23,7 +29,7 @@ func InitTracerProvider() *sdktrace.TracerProvider {
 	res, err := resource.New(
 		context.Background(),
 		resource.WithAttributes(
-			semconv.ServiceNameKey.String("fiber-go"),
+			semconv.ServiceNameKey.String(serviceName),
 		),
 	)
 	if err != nil {
